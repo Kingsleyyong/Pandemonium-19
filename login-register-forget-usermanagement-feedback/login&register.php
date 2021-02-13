e19ab7f8aad1b26f49457440ee7bcf57b9071ca0
MrYi <72812317+Mryi007@users.noreply.github.com>
2021-02-13 15:48:17 +0800

    Updated

---
+++ login-register-forget-usermanagement-feedback/login&register.php
@@ -0,0 +1,231 @@
+<?php
+session_start();
+
+	include("connect.php");  	//including connect.php to this file
+	include("function.php");      //including function.php to this file
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Log In</title>
+	<style>
+		body,
+		html {
+			margin: 0;
+			padding: 0;
+			background: #30343F;
+		}
+
+		#loginbox {
+			width: 380px;
+			height: 480px;
+			position: relative;
+			margin: 6% auto;
+			border: 1pt solid #30343F;
+			border-radius: 10px;
+			padding: 5px;
+			background-color: #E4D9FF;
+			overflow: hidden;
+		}
+
+		#ButtonBox {
+			width: 220px;
+			margin: 35px auto;
+			position: relative;
+			border-radius: 30px;
+			box-shadow: 0 0 20px 9px #8997D2;
+		}
+
+		.sButton {
+			padding: 10px 30px;
+			cursor: pointer;
+			border: none;
+			background: transparent;
+			position: relative;
+			outline: none;
+		}
+
+		#btn {
+			top: 0;
+			left: 0;
+			position: absolute;
+			width: 110px;
+			height: 100%;
+			background: linear-gradient(to right, #8997D2, #8997D2);
+			border-radius: 30px;
+			transition: .4s;
+		}
+
+		.inputGroup {
+			top: 120px;
+			position: absolute;
+			width: 280px;
+			transition: .4s;
+		}
+
+		.inputMember {
+			width: 100%;
+			padding: 10px 10px;
+			margin: 5px 0;
+			border: none;
+			border-bottom: 1px solid black;
+			background: transparent;
+		}
+
+		.LoginBtn {
+			width: 85%;
+			padding: 10px 30px;
+			cursor: pointer;
+			margin: auto;
+			margin-top: 20px;
+			margin-left: 30px;
+			border: none;
+			border-radius: 30px;
+			background: #8080ff;
+		}
+
+		.LoginBtn:hover {
+			background: #2e2eb8;
+		}
+
+		#forget {
+			font-size: 15px;
+			bottom: -30px;
+			position: absolute;
+			left: 10px;
+		}
+
+		#signin {
+			left: 50px;
+		}
+
+		#Register {
+			left: 450px;
+		}
+
+		.checkbox {
+			margin: 30px 10px 30px 0;
+		}
+	</style>
+</head>
+
+<body>
+	<div id="container">
+		<div id="loginbox">
+			<section id="ButtonBox">
+				<section id="btn"></section>
+				<button type="button" class="sButton" onclick="signin()">Log In</button>
+				<button type="button" class="sButton" onclick="Register()">Register</button>
+			</section>
+
+		<!--login form -->
+			<form id="signin" class="inputGroup" action="" method="post">
+				<input type="Email" class="inputMember" placeholder="Email" name="email" required>
+				<input type="password" class="inputMember" placeholder="Password" name="password" required>
+				<span style="padding: 10px;">Select User Type : <select name="usertype">
+					<option value="admin">admin</option>
+					<option value="user" selected>user</option>
+				</select></span>
+				<button type="submit" class="LoginBtn" name="login">Log In</button>
+				<a href="forgetpass.php" target="_blank" id="forget">Forget password</a>
+			</form>
+
+			<!-- validate username and password -->
+			<?php 
+				if($_SERVER['REQUEST_METHOD'] == "POST")
+				{
+					$userEmail = $_POST['email'];
+					$password = $_POST['password'];
+
+					if( !empty($password) && !empty($userEmail))
+					{
+						$query = "select * from user where user_email = '$userEmail' limit 1";
+
+						$result = mysqli_query($con,$query);
+
+						if($result) //if result is succesful
+						{
+							if($result && mysqli_num_rows($result)>0)
+							{
+								$user_data = mysqli_fetch_assoc($result);
+
+								if($user_data['password'] === $password)
+								{
+									if($user_data['type'] === "User")
+									{
+										$_SESSION['id'] = $user_data['id'];
+										header("Location: index.php");
+										die;
+									}
+									else if($user_data['type'] === "Admin")
+									{
+										$_SESSION['id'] = $user_data['id'];
+										header("Location: adminpage.php");
+										die;
+									}
+								}
+							}
+							else
+							{
+								?>
+								<script type="text/javascript">
+								alert("Invalid user name or password!");
+								</script>
+								<?php
+							}				
+						}
+						
+					}
+				} ?>
+
+		<!-- Register form -->
+			<form id="Register" class="inputGroup" action="register.php" method="post">
+				<input type="text" class="inputMember" placeholder="Username" name="username" required>
+				<input type="Email" class="inputMember" placeholder=" Email" name="email" required>
+				<input type="password" class="inputMember" placeholder="Password" name= "password" required id="pass">
+				<input type="password" class="inputMember" placeholder="Confirm Password" name="confirm_password" required
+					id="confirm_password">
+				<input type="checkbox" class="checkbox" required><span>I agree to the <a href="termNcondition.html"
+						target="_blank"><u>Terms & Conditions<u></a></span>
+				<button type="submit" class="LoginBtn">Register</button>
+			</form>
+			<script>
+				// for the buttton function in login and register
+				var s = document.getElementById("signin");
+				var r = document.getElementById("Register");
+				var b = document.getElementById("btn");
+
+
+				function Register() {
+					s.style.left = "-400px";
+					r.style.left = "50px";
+					b.style.left = "110px";
+				}
+				function signin() {
+					s.style.left = "50px";
+					r.style.left = "450px";
+					b.style.left = "0px";
+				}
+
+				// checking whether the password and confirm password are match
+				var password = document.getElementById("pass");
+				var confirm_password = document.getElementById("confirm_password");
+				function verifyPassword() {
+					if (password.value != confirm_password.value) {
+						confirm_password.setCustomValidity("Passwords are not match!");
+					}
+					else {
+						confirm_password.setCustomValidity('');
+					}
+				}
+				password.onchange = verifyPassword;
+				confirm_password.onkeyup = verifyPassword;
+			</script>
+
+		</div>
+	</div>
+</body>
+</html>
---
+++ login-register-forget-usermanagement-feedback/user_management.php
@@ -0,0 +1,114 @@
+<?php
+session_start();
+
+	include("connect.php");
+	include("function.php");
+
+	$user_data = check_log($con);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User Management</title>
+	<style type="text/css">
+		body,html{
+			background-color: #30343F;		
+		}
+
+		.user-container table,tr,th,td{
+			border: 1px solid black;
+			border-collapse: collapse;
+			padding: 10px;
+			border-radius: 10px;
+		}
+
+		.user-container table{
+			position: relative;
+			margin: 7% auto;
+			border-color: #30343F;
+			padding: 10px;
+			background-color: #E4D9FF;
+			text-align: center;
+		}
+	</style>
+	<script type="text/javascript">
+		function confirmation()
+		{
+			var confirmm = confirm("Are you sure you want to delete record?");
+			if(confirmm)
+			{
+				return true;
+			}
+			else
+			{
+				event.stopPropagation(); 
+				event.preventDefault();
+			}
+		}
+		
+			
+	</script>
+</head>
+<body>
+
+	<div class = "user-container">
+		<table>
+			<tr>
+				<th>ID</th>
+				<th>Username</th>
+				<th>Email</th>
+				<th>Roles</th>
+				<th colspan="3">Option</th>
+			</tr>
+			<?php 
+
+			$result = mysqli_query($con,"select * from user");
+			$numRow = mysqli_num_rows($result); //counting number of rows.
+
+			while($row = mysqli_fetch_assoc($result))
+			{
+				$userid = $row['id'];
+			?>
+			<tr>
+				<td><?php echo $row['id']?></td>
+				<td><?php echo $row['user_name']?></td>
+				<td><?php echo $row['user_email']?></td>
+				<td><?php echo $row['type']?></td>
+				<td><a href="user_detail.php?id=<?php echo $userid;?>&pageset=true">View Details</a></td>
+				<td><a href="user_detail_edit.php?id=<?php echo $userid;?>&pageedit=true">Edit Details</a></td>
+				<td><a href="?id=<?php echo $userid;?>&pagedelete=true" onclick="confirmation()" style="color: red;">Delete</a></td>
+			</tr>
+			<?php
+			}
+			?>
+		</table>
+		<footer style="color: white;">Total record found : <?php echo $numRow?></footer>
+	</div>
+</body>
+</html>
+
+<!-- for deleting user -->
+<?php 
+
+if(isset($_GET['pagedelete']))
+{
+	$id = $_GET['id'];
+
+	// if fail to delete record.
+	if($deleteResult = mysqli_query($con, "delete from user where id = $id"))
+	{
+		?>
+		<script type="text/javascript">
+		alert("You have successfull deleted a record");
+		</script>
+	<?php
+	}
+	else
+	{
+		echo "fail to delete record";
+	}
+	
+	header("refresh:0.5; url= user_management.php" );
+}
+?>
