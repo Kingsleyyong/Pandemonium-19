6d2d3c027806fe3bd2124abbd984b4c41eaba138
Kingsley Yong <kingsleyyong0208@gmail.com>
2021-02-13 09:10:12 +0800

    Changes fro Kingsley site

---
+++ main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>x | Pandemonium-19</title>
+    <link rel="shortcut icon" href="./assets/favicon.png" type="image/x-icon">  
+</head>
+<body>
+    <!-- NAV UI Import here -->
+    <?php require(""); ?> 
+
+    <iframe src="https://ourworldindata.org/grapher/total-cases-covid-19?tab=map" width="100%" height="600px"></iframe>
+
+    <!-- Footer UI Import Here -->
+    <?php require(""); ?> 
+</body>
+</html>
