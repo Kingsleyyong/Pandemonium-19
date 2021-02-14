a5dea9a2fe45e143b6d4367c67a4da61ad663e61
Jawde2000 <65705404+Jawde2000@users.noreply.github.com>
2021-02-14 20:53:28 +0800

    Update GameList.php

--- GameList.php
+++ GameList.php
@@ -2,49 +2,58 @@
 <html>
     <head>
         <title>Game List</title>
-        <link rel="shortcut icon" href="./assets/favicon.png" type="image/x-icon">
         <link rel="stylesheet" href="assets/style1.css"/>
         <!-- NAV UI Import here -->
         <?php require("navbar.html"); ?>
     </head>
     <body id="gameList">
-        <div><p id="gameBox1" name="MINI GAME" onclick=" location.href='MiniGame.html' ">MINI GAME</p></div>
+        <div><p id="gameBox1" name="MINI GAME" onclick=" location.href='MiniGame.html'">MINI GAME</p></div>
         <div><p id="gameBox2" name="TRIVIA"onclick=" location.href='Trivia.html' ">TRIVIA</p></div>
         <div><p id="gameBox3" name="QUIZ" onclick=" location.href='Quiz.html' ">QUIZ</p></div>
     </body>
+    <button id="unmute"><img src="http://upload.wikimedia.org/wikipedia/commons/2/21/Speaker_Icon.svg" id="unnmute"
+        onclick="changeImage()"></button>
 
-    <iframe src="game%20music.mp3" type="audio/mp3" allow="autoplay" style="display:none"></iframe>
-    <audio id="Audio" autoplay controls style="visibility: hidden" loop="loop">
-        <source src="game%20music.mp3" type="audio/mp3">
-    </audio>
-    <input type="checkbox" name="umute" id="umute" onclick="allow_music_on()">
-    <label for="umute" class="umute">
-        <img src="http://upload.wikimedia.org/wikipedia/commons/3/3f/Mute_Icon.svg" alt="Mute_Icon.svg"
-             title="Mute" style="position: relative; right: 770px;width: 100px;height: 100px">
-    </label>
-    <label for="umute" class="mute">
-        <img src="http://upload.wikimedia.org/wikipedia/commons/2/21/Speaker_Icon.svg"
-             alt="Speaker_Icon.svg" title="Unmute"
-        style="position: relative; right: 800px;width: 100px;height: 100px" id="mute">
-    </label>
-    
      <!-- Footer UI Import Here -->
      <?php require("footer.html"); ?>
 </html>
 
 <script>
-    var audio = document.getElementById("Audio");
+    var audio;
+    var play_button;
 
-    var Playing = true;
+function allow_music_on(){
+    audio = new Audio();
+    audio.src = "game%20music.mp3";
+    audio.loop = true;
+    audio.play();
+    play_button = document.getElementById("unmute");
+    play_button.addEventListener("click", playPause);
 
-    function allow_music_on() {
-        Playing ? audio.pause() : audio.play();
+    function playPause()
+    {
+        if(audio.paused)
+        {
+            audio.play();
+        }
+        else
+        {
+            audio.pause();
+        }
     }
 
-    audio.onplaying = function() {
-        Playing = true;
+
+}
+function changeImage()
+    {
+        if (document.getElementById("unnmute").src == "http://upload.wikimedia.org/wikipedia/commons/2/21/Speaker_Icon.svg")
+        {
+            document.getElementById("unnmute").src = "http://upload.wikimedia.org/wikipedia/commons/3/3f/Mute_Icon.svg";
+        }
+        else
+        {
+            document.getElementById("unnmute").src = "http://upload.wikimedia.org/wikipedia/commons/2/21/Speaker_Icon.svg";
+        }
     }
-    audio.onpause = function() {
-        Playing = false;
-    }
+    window.addEventListener("load", allow_music_on);
 </script>
