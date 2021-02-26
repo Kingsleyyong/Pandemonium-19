63454bdf50e281581e17d96f71e4b512075942ca
mattlab7 <43501106+mattlab7@users.noreply.github.com>
2021-02-26 13:49:34 +0800

    Styling airdrop 3

--- Merchandise Page/CartPage.php
+++ Merchandise Page/CartPage.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="shortcut icon" href="./assets/favicon.png" type="image/x-icon">
     <title>Document</title>
     
@@ -39,44 +40,68 @@
     <?php require("navbar.html"); ?> 
 </head>
 
-<body>
-    <!-- This is the items that inside the cart,scrollable -->
-    <div>
-        <img id="" src="" alt="">
-        <h2>Cotton Mask</h2>
-        <p>RM 5</p>
-        <label for="quantity">Quantity: </label>
-        <button onclick="minus()">-</button>
-        <span id="quantityValue">0</span>
-        <button onclick="plus()">+</button>
-        <br>
-    </div>
+<body class="bg-dark">
+    <div class="container">
+        <!-- This is the items that inside the cart,scrollable -->
+        <div class="row rounded bg-secondary text-light p-4">
+            <div id="col-sm">
+                <img src="../assets/cloth mask.png" class="rounded mx-auto d-block" width="200px" alt="">
+            </div>
+            <div class="col-sm mx-4">
+                <h1>Cotton Mask</h1>
+                <div class="rounded bg-info p-2 my-3">
+                    <p class="font-weight-bold" id="itemPrice">RM 5</p>
+                    <p id="itemDescription">Here is the item description.</p>
+                    <br>
+                    
+                    <label for="stockLeft">Stock Left: </label>
+                </div>
+                <form action="" class="pb-4">
+                    <label for="choices">Color: </label>
+                    <select name="maskChoices" class="form-control" id="choice">
+                        <option value=""></option>
+                    </select>
+                </form>
+                <div>
+                    <label for="quantity">Quantity: </label>
+                    <button class="btn btn-danger" onclick="minus()">-</button>
+                    <span id="quantityValue"></span>
+                    <button class="btn btn-success" onclick="plus()">+</button>
+                </div>
+                <br>
+                <input type="submit" class="btn btn-primary" value="Add To Cart">
+            </div>
+        </div>
+    
 
-    <!-- Below is Purchase Summary side bar which was not scrollable -->
-    <div>
-        <p>
-            Shipping Address: <br>
-            <textarea name="shipAddress" id="addressInput" cols="30" rows="5"
-            placeholder="Please enter your address." onchange="errorMessage()"></textarea>
-        </p>
-
-        <p>
-            Merchandse Subtotal:
-            <span id="merchandSubtotal"></span>
-        </p>
-
-        <p>
-            Shipping Subtotal:
-            <span id="shipSubtotal"></span>
-        </p>
-
-        <p>
-            Total Payment: 
-            <span id="total"></span>
-        </p>
-
-        <p> <input type="submit" value="Checkout" onclick="checkoutButton()"> </p>
-
+        <!-- Below is Purchase Summary side bar which was not scrollable -->
+        <div class="row rounded bg-info text-light p-4">
+            <div class="container">
+                <div class="row">
+                    <p class="col-sm">
+                        Shipping Address: <br>
+                        <textarea name="shipAddress" id="addressInput" cols="30" rows="5"
+                        placeholder="Please enter your address." onchange="errorMessage()"></textarea>
+                    </p>
+                    <p class="col-sm">
+                        Merchandse Subtotal: <br>
+                        <span id="merchandSubtotal"></span>
+                    </p>
+        
+                    <p class="col-sm">
+                        Shipping Subtotal: <br>
+                        <span id="shipSubtotal"></span>
+                    </p>
+                </div>
+                <div class="row">
+                    <p class="col-md">
+                        Total Payment: <br>
+                        <span id="total"></span>
+                    </p>
+                    <p class="col-sm"> <input type="submit" class="btn btn-primary" value="Checkout" onclick="checkoutButton()"> </p>
+                </div>
+            </div>
+        </div>
     </div>
     <!-- Footer UI Import Here -->
     <?php require("footer.html"); ?>
--- Merchandise Page/ItemDescPage.php
+++ Merchandise Page/ItemDescPage.php
@@ -4,48 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./assets/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Item Description Page</title>
 </head>
-<body>
-    <div id="logoGroup">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="main.php">PANDEMONIUM-19</a>
-            <a href="CartPage.php"><img src="" alt="Cart"></a>
-        </ul>
-    </div>
-
-    <div id="picGroup">
-        <div id="multiPic">
-            <ul>
-                <li class="picSelection"><img src="" alt=""></li>
-                <li class="picSelection"><img src="" alt=""></li>
-                <li class="picSelection"><img src="" alt=""></li>
-            </ul>
+<body >
+    <div class="container">
+        <div class="row rounded bg-dark text-light p-4">
+            <div id="col-sm">
+                <img src="../assets/cloth mask.png" class="rounded mx-auto d-block" width="200px" alt="">
+            </div>
+            <div class="col-sm mx-4">
+                <h1>Cotton Mask</h1>
+                <div class="rounded bg-info p-2 my-3">
+                    <p class="font-weight-bold" id="itemPrice">RM 5</p>
+                    <p id="itemDescription">Here is the item description.</p>
+                    <br>
+                    
+                    <label for="stockLeft">Stock Left: </label>
+                </div>
+                <form action="" class="pb-4">
+                    <label for="choices">Color: </label>
+                    <select name="maskChoices" class="form-control" id="choice">
+                        <option value=""></option>
+                    </select>
+                </form>
+                <div>
+                    <label for="quantity">Quantity: </label>
+                    <button class="btn btn-danger" onclick="minus()">-</button>
+                    <span id="quantityValue"></span>
+                    <button class="btn btn-success" onclick="plus()">+</button>
+                </div>
+                <br>
+                <input type="submit" class="btn btn-primary" value="Add To Cart">
+            </div>
         </div>
-        <div id="mainPic"><img src="images/cloth mask.png" alt=""></div>
-    </div>
-
-    <div id="descriptionGroup">
-        <h1>Cotton Mask</h1>
-        <p id="itemPrice">RM 5</p>
-        <p id="itemDescription">Here is the item description.</p>
-        <br>
-        <label for="stockLeft">Stock Left: </label>
-
-        <form action="">
-            <label for="choices">Color: </label>
-            <select name="maskChoices" id="choice">
-                <option value=""></option>
-            </select>
-        </form>
-
-        <label for="quantity">Quantity: </label>
-        <button onclick="minus()">-</button>
-        <span id="quantityValue"></span>
-        <button onclick="plus()">+</button>
-        <br>
-
-        <input type="submit" value="Add To Cart">
     </div>
     
     <script>
