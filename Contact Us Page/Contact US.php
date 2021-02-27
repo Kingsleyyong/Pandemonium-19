1e9f54e86c0a6cfb773c05a1ad5f91d4c0e1234e
mattlab7 <43501106+mattlab7@users.noreply.github.com>
2021-02-27 15:11:26 +0800

    contact us styling

--- Contact Us Page/Contact US.php
+++ Contact Us Page/Contact US.php
@@ -3,49 +3,63 @@
 <head>
     <meta charset="UTF-8">
     <title>Contact Us</title>
-    <link href="../assets/CSS/Contact Us.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!--<link href="../assets/CSS/Contact Us.css" rel="stylesheet">-->
 </head>
-<body style="margin: auto;display: block;width: 600px">
-
-<form action="" name="Contact form" method="post">
-    <div class="contact_mail" style="padding-bottom: 30px">
-        <p>Contact Us for support or any question at <a href="">www@.gmail.com</a></p> <!--hyperlink for our gmail-->
-        <p>GET IN TOUCH AND WE WILL BE IN TOUCH AS SOON AS POSSIBLE</p>
+<body class="bg-dark text-light">
+    <div class="col">
+        <p class="text-center">Contact Us for support or any question at <a href="mailto:www@gmail.com">www@gmail.com</a></p> <!--hyperlink for our gmail-->
+        <p class="text-center">GET IN TOUCH AND WE WILL BE IN TOUCH AS SOON AS POSSIBLE</p>
     </div>
-    <div class="contact_mail">
-        <label>First Name<span class="asterisk">*</span></label><!--do the asterisk red colour-->
-        <input type="text" name="fname" id="fname" size="30" required>
-        <!-- first name -->
-        <label style="position: relative;right: 70px">Last Name<span class="asterisk">*</span></label>
-        <!-- last name -->
-        <!--do the asterisk red colour-->
-        <input type="text" name="lname" id="lname" size="23" required>
-    </div>
-    <div class="contactmail2">
-        <label>Your Email<span class="asterisk">*</span></label>
-        <input type="email" name="email" id="email" size="60" required>
-        <!-- email -->
-    </div>
-    <div class="contactmail3">
-        <label>Phone Numbers</label>
-        <input type="tel" name="phone" id="phone" size="60">
-        <!-- phone number -->
-    </div>
-    <div class="contactmail4">
-        <label id="messa">Message</label>
-        <textarea name="message" id="message" rows="5" cols="56" placeholder="Leave your message here..."></textarea>
-        <!-- message -->
-    </div>
-    <div id="submission">
-        <input type="submit" name="Submission" value="Submit" id="co" >
-    </div>
-    <div >
-        <p><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15938.441917421716!2d101.6419004!3d2.9277715!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd0f74e8ad10f1129!2sMultimedia%20University%20-%20MMU%20Cyberjaya!5e0!3m2!1sen!2smy!4v1611408350513!5m2!1sen!2smy"
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <form action="" name="Contact form" method="post">
+                    <div class="container form-group">
+                        <div class="row mb-3">
+                            <div class="col">
+                                <label>First Name<span>*</span></label><!--do the asterisk red colour-->
+                                <input type="text" class="form-control" name="fname" id="fname" size="30" required>
+                            </div>
+                            <div class="col">
+                                <label>Last Name<span class="asterisk">*</span></label>
+                                <input type="text" class="form-control" name="lname" id="lname" size="23" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <label>Your Email<span class="asterisk">*</span></label>
+                                <input type="email" class="form-control" name="email" id="email" size="60" required> 
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <label>Phone Numbers</label>
+                                <input type="tel" class="form-control" name="phone" id="phone" size="60">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <label id="messa">Message</label>
+                                <textarea name="message" class="form-control" id="message" rows="5" cols="56" placeholder="Leave your message here..."></textarea>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <input type="submit" class="btn btn-primary" name="Submission" value="Submit" id="co">  
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15938.441917421716!2d101.6419004!3d2.9277715!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd0f74e8ad10f1129!2sMultimedia%20University%20-%20MMU%20Cyberjaya!5e0!3m2!1sen!2smy!4v1611408350513!5m2!1sen!2smy"
                 width="250" height="250" frameborder="0"
-                style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" id="map"></iframe></p>
-        <p id="location">Persiaran Multimedia, 63100 Cyberjaya, Selangor</p>
+                style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" id="map" class="d-block mx-auto"></iframe>
+                <p class="text-center">Persiaran Multimedia, 63100 Cyberjaya, Selangor</p>
+            </div>
+        </div>
     </div>
-</form>
 </body>
 </html>
 
