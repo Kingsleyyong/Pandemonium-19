9b99bf0a705cd782f11e1eb2057c9d65f27a0c8a
Kingsleyyong <kingsleyyong0208@gmail.com>
2021-02-28 16:30:08 +0800

    edit by kingsley

--- Merchandise Page/CartPage.php
+++ Merchandise Page/CartPage.php
@@ -7,29 +7,6 @@
     <link rel="shortcut icon" href="./assets/favicon.png" type="image/x-icon">
     <title>Add to Cart</title>
     
-    <script>
-        window.addEventListener('DOMContentLoaded', errorMessage);
-
-        function errorMessage(){
-            var length = document.getElementsByTagName('textarea')['shipAddress'].innerHTML.length;
-            if(length==0){
-                document.getElementById('shipSubtotal').style.cssText='color: red; font-weight: bold;';
-                document.getElementById('total').style.cssText='color: red; font-weight: bold;';
-                document.getElementById('shipSubtotal').innerHTML='Please enter your address.';
-                document.getElementById('total').innerHTML='Please enter your address.';
-                
-            }
-            else{
-                var shippingFees = length*0.5;
-                var total = <?php echo json_encode($merchantTotal); ?>;
-                console.log(total);
-                document.getElementById('shipSubtotal').innerHTML='RM '+shippingFees.toFixed(2);
-                document.getElementById('total').innerHTML='RM '+total.toFixed(2);
-            } 
-        }
-        
-    </script>
-    
     <!-- NAV UI Import here -->
     <?php require("../Navigation Bar and Footer/navbar.php") ?>
 </head>
@@ -50,7 +27,6 @@
         foreach($result as $row):
             $cartID = $row ['cartID'];
             $address = $row ['shippingAddress'];
-            // $address = "";
 
             $result2 = mysqli_query($con,"SELECT itemID, quantity FROM cartrecord WHERE cartID='$cartID'");
             foreach($result2 as $row2):
@@ -68,6 +44,8 @@
                     $image = '<img class="rounded mx-auto d-block" width="200px" alt="" width = 150dp 
                     height = 130dp src="data:image/jpeg;base64,'.base64_encode( $row3['image'] ).'"/>';
                     $merchantTotal += $itemPrice;
+                    $shippingSubtotal = strlen($address)*0.5;
+                    $total = $merchantTotal + $shippingSubtotal;
 ?>
                     <div class="col">
                         <?php echo $image; ?>            
@@ -83,10 +61,7 @@
 
                         <label for="quantity">Quantity: <?php echo $itemAmount; ?></label>
 
-                    </div>
-
-                    
-                    
+                    </div>                
                 </div>
                 </div>
                 </div>
@@ -98,8 +73,7 @@
         endforeach;        
     }
 ?>
-                
-            </div>
+                </div>
         </div>  
 
         <!-- Below is Purchase Summary side bar which was not scrollable -->
@@ -109,7 +83,7 @@
                     <div class="row">
                         <p class="col-sm">
                             Shipping Address: <br>
-                            <textarea name="shipAddress" id="addressInput" cols="30" rows="5" placeholder="Please enter your address." onchange='errorMessage()'><?php echo $address;?></textarea>
+                            <span id='shipAddress' name='shipAddress'> <?php echo $address ?></span>
                         </p>
                         <p class="col-sm">
                             Merchandse Subtotal: <br>
@@ -117,17 +91,22 @@
                         </p>
             
                         <p class="col-sm">
-                            Shipping Subtotal: <br>
-                            <span id="shipSubtotal" ></span>
+                            Shipping Subtotal: <br> 
+                            <span id="shipSubtotal" >RM <?php echo number_format($shippingSubtotal, 2, '.', ''); ?></span>
+                        </p>
+                        <p class="col-md">
+                            Total Payment: <br> 
+                            <span id="total">RM <?php echo number_format($total, 2, '.', ''); ?></span>
                         </p>
                     </div>
-                    <div class="row">
-                        <p class="col-md">
-                            Total Payment: <br>
-                            <span id="total"></span>
+                    <div class="row justify-content-center">
+                        <p class="col-sm"> 
+                            <div class="m-3"><input type="submit" class="btn btn-secondary" value="Edit Address" onclick=""></div>
+                            <div class="m-3"><input type="submit" class="btn btn-primary btn-lg" value="Checkout" onclick="checkoutButton"></div>
+                            <div class="m-3"><input type="submit" class="btn btn-secondary" value="Delete Item" onclick=""> </div>
                         </p>
                         <p class="col-sm"> 
-                            <input type="submit" class="btn btn-primary" value="Checkout" onclick="checkoutButton"> 
+                             
                         </p>
                     </div>
                 </form>
@@ -139,6 +118,22 @@
 </body>
 </html>
 
-<?php 
+<!-- Store data into databse-->
+ <?php 
+     if(!$_POST('checkoutBtn')){}else{
+         $sql = 'UPDATE cart 
+                 SET shippingAddress = "$shipAddress", shippingFee = "$shipSub", subTotal = "$subTotal", TotalPayment = "$total" 
+                 WHERE cartID = "$cartID" AND userID = "$userID"';
 
-?>
+         if($sqlResult){
+             $sqlResult = mysqli_query($con, $sql);?>
+             <script>alert("Done, please proceed to checkout at our partner bank.")</script>
+             <?php }else{?>
+             <script>alert("Error.")</script>    
+ <?php
+         }
+     }
+
+
+
+ ?> 
