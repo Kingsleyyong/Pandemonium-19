d4a7a0da2dd6433bd5c9d6e1fe2f8f5007177f4c
mattlab7 <43501106+mattlab7@users.noreply.github.com>
2021-02-28 14:45:33 +0800

    cart page

--- Merchandise Page/CartPage.php
+++ Merchandise Page/CartPage.php
@@ -38,8 +38,7 @@
     <div class="container">
         <!-- This is the items that inside the cart,scrollable -->
         <div class="row rounded bg-secondary text-light p-4">
-            <div class="col-sm mx-4">
-                <div class="rounded bg-info p-2 my-3">
+            <div class="col">
 <?php
     require ('../Database/connect.php');
     $merchantTotal = 0;
@@ -55,6 +54,7 @@
 
             $result2 = mysqli_query($con,"SELECT itemID, quantity FROM cartrecord WHERE cartID='$cartID'");
             foreach($result2 as $row2):
+                echo '<div class="rounded bg-info p-2 my-3"><div class="container"><div class="row">';
                 $itemID = $row2['itemID'];
                 $itemAmount = $row2['quantity'];
 
@@ -69,17 +69,28 @@
                     height = 130dp src="data:image/jpeg;base64,'.base64_encode( $row3['image'] ).'"/>';
                     $merchantTotal += $itemPrice;
 ?>
-                    <?php echo $image; ?>            
+                    <div class="col">
+                        <?php echo $image; ?>            
+                    </div>
+                    <div class="col">
+                        <h2> <?php echo $itemName; ?> </h2>
 
-                    <h2> <?php echo $itemName; ?> </h2>
+                        <p class="font-weight-bold" id="itemPrice">RM <?php echo $itemPrice; ?></p>
 
-                    <p class="font-weight-bold" id="itemPrice">RM <?php echo $itemPrice; ?></p>
+                        <label for="choices">Color: <?php echo $itemColour; ?> </label><br>
+
+                        <label for="size">Size: <?php echo $itemSize; ?> </label><br>
+
+                        <label for="quantity">Quantity: <?php echo $itemAmount; ?></label>
+
+                    </div>
+
                     
-                    <label for="choices">Color: <?php echo $itemColour; ?> </label><br>
+                    
+                </div>
+                </div>
+                </div>
 
-                    <label for="size">Size: <?php echo $itemSize; ?> </label><br>
-
-                    <label for="quantity">Quantity: <?php echo $itemAmount; ?></label>
 
 <?php
                 endforeach;
@@ -87,7 +98,7 @@
         endforeach;        
     }
 ?>
-                </div>
+                
             </div>
         </div>  
 
@@ -102,21 +113,21 @@
                         </p>
                         <p class="col-sm">
                             Merchandse Subtotal: <br>
-                            <span id="merchandSubtotal" name="merchandSub">RM <?php echo number_format($merchantTotal, 2, '.', ''); ?></span>
+                            <span id="merchandSubtotal" name="merchandSubtotal">RM <?php echo number_format($merchantTotal, 2, '.', ''); ?></span>
                         </p>
             
                         <p class="col-sm">
                             Shipping Subtotal: <br>
-                            <span id="shipSubtotal" name="shipSub"></span>
+                            <span id="shipSubtotal" ></span>
                         </p>
                     </div>
                     <div class="row">
                         <p class="col-md">
                             Total Payment: <br>
-                            <span id="total" name="total"></span>
+                            <span id="total"></span>
                         </p>
                         <p class="col-sm"> 
-                            <input type="submit" class="btn btn-primary" name="checkoutBtn" value="Checkout" > 
+                            <input type="submit" class="btn btn-primary" value="Checkout" onclick="checkoutButton"> 
                         </p>
                     </div>
                 </form>
@@ -128,27 +139,6 @@
 </body>
 </html>
 
-<!-- Store data into databse-->
 <?php 
-    $shipAddress = $_POST['shipAddress'];
-    $shipSub = $_POST['shipSub'];
-    $subTotal = $_POST['merchandSub'];
-    $total = $_POST['total'];
-
-    if(!$_POST('checkoutBtn')){}else{
-        $sql = 'UPDATE cart 
-                SET shippingAddress = "$shipAddress", shippingFee = "$shipSub", subTotal = "$subTotal", TotalPayment = "$total" 
-                WHERE cartID = "$cartID" AND userID = "$userID"';
-
-        if($sqlResult){
-            $sqlResult = mysqli_query($con, $sql);?>
-            <script>alert("Done, please proceed to checkout at our partner bank.")</script>
-            <?php }else{?>
-            <script>alert("Error.")</script>    
-<?php
-        }
-    }
-    
-
 
 ?>
