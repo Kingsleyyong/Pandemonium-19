804134a83299fdb566dc182b95169084e887eedc
LynChee1229 <1191202546@student.mmu.edu.my>
2021-03-02 08:17:14 +0800

    .

--- Testimonial Page/mostView.php
+++ Testimonial Page/mostView.php
@@ -46,6 +46,7 @@
         $per_page = 4; //the page we want per page
 
         $query = "SELECT * FROM story";
+        
         $result = mysqli_query($conn, $query) or die( mysqli_error($conn));
         $number_of_result = mysqli_num_rows($result);       //used to count number of rows results
         $num_page = ceil($number_of_result/$per_page);      //to know the number page available
@@ -74,7 +75,6 @@
                 <?php 
                     if($row['storyMedia']!=null) {
                         $pic = $row['storyMedia'];
-                        $viewNum = $row['storyView'];
                         echo '<img class="mx-auto d-block" width = "350dp" height="240dp" src="data:image/*;base64,'.
                                 base64_encode($row['storyMedia']).'" alt=""/>';
                     }
@@ -84,7 +84,6 @@
                 ?>
                 <p class="text-center"><span id="title1"><?php echo $row['storyTitle'];?></span></p>
                 <p class="text-center">Read More</p></a>
-                <p class="text-center"><< Number of Views : <?php echo $viewNum; ?> >> </p>
             </div>
             <?php
         }   ?>
